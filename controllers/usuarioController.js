0f2f0a23141d4ea6fcab92308a55c9a3b382f5eb
santiagoSuarez219 <santiago8628@gmail.com>
2024-02-13 14:23:56 -0500

    Añadir funcionalidad de registro de usuario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,6 +1,13 @@
-const registrar = (req, res) => {
-  console.log(req.body);
-  res.json({ msg: "Usuario registrado" });
+import Usuario from "../models/Usuario.js";
+
+const registrar = async (req, res) => {
+  try {
+    const usuario = new Usuario(req.body);
+    const usuarioAlmacenado = await usuario.save();
+    res.json({ msg: "Usuario registrado" });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export { registrar };
