089a065142b3867475a6c88d67497365638079b5
santiagoSuarez219 <santiago8628@gmail.com>
2024-02-15 10:52:06 -0500

    Añadir controladores de intervención y actualizar esquema de intervenciones

---
+++ controllers/intervencionControllers.js
@@ -0,0 +1,9 @@
+const obtenerIntervenciones = async (req, res) => {};
+
+const obtenerIntervencion = async (req, res) => {};
+
+const nuevaIntervencion = async (req, res) => {};
+
+const editarIntervencion = async (req, res) => {};
+
+const eliminarIntervencion = async (req, res) => {};
--- models/Intervenciones.js
+++ models/Intervenciones.js
@@ -1,32 +1,41 @@
 import mongoose from "mongoose";
 
-const intervencionesSchema = mongoose.Schema({
-  nombre: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
+const intervencionesSchema = mongoose.Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    fecha_inicio: {
+      type: Date,
+      required: true,
+    },
+    jerarquia: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Jerarquia",
+    },
+    descripcion: {
+      type: String,
+    },
+    proyecto_estrategico: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ProyectoEstrategico",
+    },
+    sistema_interno: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "SistemaInterno",
+    },
+    comentario: {
+      type: String,
+    },
   },
-  fecha_inicio: {
-    type: Date,
-    required: true,
-  },
-  jerarquia: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Jerarquia",
-  },
-  descripcion: {
-    type: String,
-  },
-  proyecto_estrategico: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "ProyectoEstrategico",
-  },
-  sistema_interno: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "SistemaInterno",
-  },
-  comentario: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+const Intervencion = mongoose.model("Intervencion", intervencionesSchema);
+
+export default Intervencion;
