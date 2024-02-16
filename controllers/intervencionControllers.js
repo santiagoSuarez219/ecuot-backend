00563a0144f501fbb3d12ce64061ac9e797b3cb5
santiagoSuarez219 <santiago8628@gmail.com>
2024-02-16 10:16:52 -0500

    Añadiendo funcion editar intervencion

--- controllers/intervencionControllers.js
+++ controllers/intervencionControllers.js
@@ -2,7 +2,9 @@ import Intervencion from "../models/Intervencion.js";
 
 const obtenerIntervenciones = async (req, res) => {
   try {
-    const intervenciones = await Intervencion.find();
+    const intervenciones = await Intervencion.find()
+      .populate("creado_por")
+      .populate("actualizado_por");
     res.status(200).json(intervenciones);
   } catch (error) {
     console.log(error);
@@ -15,6 +17,7 @@ const obtenerIntervencion = async (req, res) => {};
 const nuevaIntervencion = async (req, res) => {
   const intervencion = new Intervencion(req.body);
   intervencion.creado_por = req.usuario.id;
+  intervencion.actualizado_por = req.usuario.id;
   try {
     const intervencionAlmacenada = await intervencion.save();
     res.status(201).json(intervencionAlmacenada);
@@ -24,7 +27,29 @@ const nuevaIntervencion = async (req, res) => {
   }
 };
 
-const editarIntervencion = async (req, res) => {};
+const editarIntervencion = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const intervencion = await Intervencion.findById(id);
+    if (!intervencion) {
+      return res.status(404).json({ msg: "Intervencion no encontrada" });
+    }
+    intervencion.nombre = req.body.nombre || intervencion.nombre;
+    intervencion.fecha_inicio =
+      req.body.fecha_inicio || intervencion.fecha_inicio;
+    intervencion.jerarquia = req.body.jerarquia || intervencion.jerarquia;
+    intervencion.descripcion = req.body.descripcion || intervencion.descripcion;
+    intervencion.proyecto_estrategico =
+      req.body.proyecto_estrategico || intervencion.proyecto_estrategico;
+    intervencion.sistema_interno =
+      req.body.sistema_interno || intervencion.sistema_interno;
+    intervencion.actualizado_por = req.usuario.id;
+    const intervencionActualizada = await intervencion.save();
+    res.status(200).json(intervencionActualizada);
+  } catch (error) {
+    res.status(400).json({ msg: "Hubo un error" });
+  }
+};
 
 const eliminarIntervencion = async (req, res) => {};
 
