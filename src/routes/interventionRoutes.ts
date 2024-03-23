004ef7d37f146d2f7daacfe7f23a917ca4814fc6
santiagoSuarez219 <santiago8628@gmail.com>
2024-03-23 18:20:16 -0500

    Agrega validación de entrada y mensajes de error en las rutas de intervención

--- src/routes/interventionRoutes.ts
+++ src/routes/interventionRoutes.ts
@@ -1,9 +1,21 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import { InterventionController } from '../controllers/InterventionController';
+import { handleInputErrors } from '../middleware/validation';
 
 const router = Router();
 
-router.post('/', InterventionController.createIntervention);
+//TODO: Implementar el resto de validacion de datos
+router.post('/', 
+    body('interventionName')
+        .notEmpty().withMessage('El nombre de la intervención es requerido'),
+    body('description')
+        .notEmpty().withMessage('La descripción es requerida'),
+    body('strategicProject')
+        .notEmpty().withMessage('El proyecto estratégico es requerido'),
+    handleInputErrors,
+    InterventionController.createIntervention
+);
 router.get('/', InterventionController.getAllInterventions);
 
 export default router;
