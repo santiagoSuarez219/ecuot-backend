adf7570ae1d46ff611e3203ec50f73cb07724ce4
santiagoSuarez219 <santiago8628@gmail.com>
2024-03-23 18:40:24 -0500

    CRUD intervenciones

--- src/controllers/InterventionController.ts
+++ src/controllers/InterventionController.ts
@@ -2,17 +2,64 @@ import type { Request, Response } from "express";
 import Intervention from "../models/Intervention";
 
 export class InterventionController {
-    static createIntervention = async (req: Request, res: Response) => {
-        const intervention = new Intervention(req.body);
-        try {
-            await intervention.save();
-            res.send("Intervencion creada correctamente")
-        } catch (error) {
-            console.log(error);
-        }
+  static createIntervention = async (req: Request, res: Response) => {
+    const intervention = new Intervention(req.body);
+    try {
+      await intervention.save();
+      res.send("Intervencion creada correctamente");
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    static getAllInterventions = async (req: Request, res: Response) => {
-        res.send("Todos las intervenciones");
+  static getInterventionById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const intervention = await Intervention.findById(id);
+      if (!intervention) {
+        return res.status(404).json({ message: "Intervencion no encontrada" });
+      }
+      res.json(intervention);
+    } catch (error) {
+      console.log(error);
     }
+  };
+
+  static getAllInterventions = async (req: Request, res: Response) => {
+    try {
+      const interventions = await Intervention.find({});
+      res.json(interventions);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  static updateIntervention = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const intervention = await Intervention.findByIdAndUpdate(id, req.body);
+      if (!intervention) {
+        return res.status(404).json({ message: "Intervencion no encontrada" });
+      }
+      await intervention.save();
+      res.json("Intervencion actualizada correctamente");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  static deleteIntervention = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const intervention = await Intervention.findById(id);
+      if (!intervention) {
+        return res.status(404).json({ message: "Intervencion no encontrada" });
+      }
+      // TODO: Realizar validaciones de seguridad
+      await intervention.deleteOne();
+      res.json("Intervencion eliminada correctamente");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
--- src/routes/interventionRoutes.ts
+++ src/routes/interventionRoutes.ts
@@ -1,21 +1,51 @@
-import { Router } from 'express';
-import { body } from 'express-validator';
-import { InterventionController } from '../controllers/InterventionController';
-import { handleInputErrors } from '../middleware/validation';
+import { Router } from "express";
+import { body, param } from "express-validator";
+import { InterventionController } from "../controllers/InterventionController";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
 
 //TODO: Implementar el resto de validacion de datos
-router.post('/', 
-    body('interventionName')
-        .notEmpty().withMessage('El nombre de la intervención es requerido'),
-    body('description')
-        .notEmpty().withMessage('La descripción es requerida'),
-    body('strategicProject')
-        .notEmpty().withMessage('El proyecto estratégico es requerido'),
-    handleInputErrors,
-    InterventionController.createIntervention
+router.post(
+  "/",
+  body("interventionName")
+    .notEmpty()
+    .withMessage("El nombre de la intervención es requerido"),
+  body("description").notEmpty().withMessage("La descripción es requerida"),
+  body("strategicProject")
+    .notEmpty()
+    .withMessage("El proyecto estratégico es requerido"),
+  handleInputErrors,
+  InterventionController.createIntervention
 );
-router.get('/', InterventionController.getAllInterventions);
+router.get("/", InterventionController.getAllInterventions);
+
+router.get(
+  "/:id",
+  param("id").isMongoId().withMessage("El id de la intervención no es válido"),
+  handleInputErrors,
+  InterventionController.getInterventionById
+);
+
+router.put(
+  "/:id",
+  param("id").isMongoId().withMessage("El id de la intervención no es válido"),
+  body("interventionName")
+    .notEmpty()
+    .withMessage("El nombre de la intervención es requerido"),
+  body("description").notEmpty().withMessage("La descripción es requerida"),
+  body("strategicProject")
+    .notEmpty()
+    .withMessage("El proyecto estratégico es requerido"),
+  handleInputErrors,
+  InterventionController.updateIntervention
+);
+
+router.delete(
+  "/:id",
+  param("id").isMongoId().withMessage("El id de la intervención no es válido"),
+  handleInputErrors,
+  InterventionController.deleteIntervention
+);
 
 export default router;
