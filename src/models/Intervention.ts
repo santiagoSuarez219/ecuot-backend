5851dc04ab41236af7fbf63ab3a40bf990fa02d7
santiagoSuarez219 <santiago8628@gmail.com>
2024-03-23 20:21:41 -0500

    Añadir campo de jerarquía y relación con conflictos en el modelo de intervención

--- src/models/Intervention.ts
+++ src/models/Intervention.ts
@@ -1,45 +1,67 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document, PopulatedDoc } from "mongoose";
+import { IConflict } from "./Conflict";
+
+const hierarchy = {
+  CIUDAD: "Ciudad",
+  BARRIAL_SUBURBANO: "BarrialSuburbano",
+  ZONA_CORREGIMENTAL: "ZonaCorregimental",
+  METROPOLITANO_REGIONAL: "metropolitanoRegional",
+} as const;
+
+export type Hierarchy = (typeof hierarchy)[keyof typeof hierarchy];
 
 export interface IIntervention extends Document {
   interventionName: string;
-  hierarchy: string; // Jerarquia
+  hierarchy: Hierarchy; // Jerarquia
   description: string;
   strategicProject: string; // Proyecto estratégico
-  internalSystem: string;
-  startDate: Date;
+  //internalSystem: string;
+  //startDate: Date;
+  conflicts: PopulatedDoc<IConflict & Document>[];
   // createdBy: Id de usuario
-};
+}
 
-const InterventionSchema = new Schema({
-  interventionName: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
+//TODO: Descomentar los campos que se necesiten e incluir relaciones con otras entidades
+//TODO: Relacionar con Noticias
+const InterventionSchema = new Schema(
+  {
+    interventionName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    hierarchy: {
+      type: String,
+      enum: Object.values(hierarchy),
+      // required: true,
+      default: hierarchy.CIUDAD, //TODO: Eliminar parametro por defecto cuando se implemente en el frontend
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    strategicProject: {
+      type: String,
+      required: true,
+    },
+    // internalSystem: {
+    //   type: String,
+    //   required: true,
+    // },
+    // startDate: {
+    //   type: Date,
+    //   required: true,
+    // },
+    // createdBy: {type: Schema.Types.ObjectId, ref: 'User'}
+    conflicts: [{ type: Schema.Types.ObjectId, ref: "Conflict" }],
   },
-  hierarchy: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  strategicProject: {
-    type: String,
-    required: true,
-  },
-  internalSystem: {
-    type: String,
-    required: true,
-  },
-  startDate: {
-    type: Date,
-    required: true,
-  },
-  // createdBy: {type: Schema.Types.ObjectId, ref: 'User'}
-});
+  { timestamps: true }
+);
 
-const Intervention = mongoose.model<IIntervention>("Intervention", InterventionSchema);
+const Intervention = mongoose.model<IIntervention>(
+  "Intervention",
+  InterventionSchema
+);
 
 export default Intervention;
